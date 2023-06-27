8e9b6c2d5ed50397b34df8474698b037f6288f04
bkcjanta <bhupendrafbook@gmail.com>
2023-06-28 01:48:42 +0530

    Backend issue fixed

--- models/booking.model.js
+++ models/booking.model.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 // create a schema for flights bookings by users
 
 const bookingSchema = mongoose.Schema({
-    flightId: { type: mongoose.Schema.Types.ObjectId, ref: "trips", required: true },
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
+    flightId: { type: mongoose.Schema.Types.ObjectId, ref: "flights", required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
     seats: { type: Number, required: true },
     totalPrice: { type: Number, required: true },
     bookingDate: { type: Date, default: Date.now },
